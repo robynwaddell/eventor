cb86ff9f5fcf2a8d64a256b169f9662000383e5d
Robyn Waddell <retwaddell@shaw.ca>
2024-06-20 09:12:48 -0500

    minor alterations

--- frontend/eventor/src/components/Header.js
+++ frontend/eventor/src/components/Header.js
@@ -13,7 +13,6 @@ function Header() {
           <li><NavLink className={({isActive}) => (isActive ? 'active' : '')}to='/hosts'>Find a Host</NavLink></li>
           <li><NavLink className={({isActive}) => (isActive ? 'active' : '')} to='/create-event'>Create an Event</NavLink></li>
           <li><NavLink className={navData => (navData.isActive ? 'active' : '')} to='/login'>Login</NavLink></li>
-          <li><NavLink className={({isActive}) => (isActive ? 'active' : '')} to='/profile'><i class="fa-solid fa-user"></i></NavLink></li>
         </ul>
       </nav>
     </header>
