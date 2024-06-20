77b3decf6d018de95d2ec334e24040fede8bc1f4
XiaoFang <yangxiaofang82@gmail.com>
2024-06-20 10:47:19 -0500

    add fetching funcions in Hosts and Events

--- frontend/eventor/src/App.js
+++ frontend/eventor/src/App.js
@@ -10,14 +10,11 @@ import Editevent from './pages/Editevent';
 import Login from './pages/Login';
 
 import Denied from './pages/Denied';
-<<<<<<< HEAD
 import Hostevent from './pages/Hostevent';
 import Becometohost from './pages/Becometohost';
-=======
 import LoginSignUp from './pages/LoginSignUp';
 import Event from './pages/Event';  
 import Host from './pages/Host';  
->>>>>>> cb86ff9f5fcf2a8d64a256b169f9662000383e5d
 
 function App() {
   return (
@@ -30,7 +27,6 @@ function App() {
           <Route path='/events' element={<Events />}/>
           <Route path='/hosts' element={<Hosts />}/>
           <Route path="/login" element={<Login />} />
-<<<<<<< HEAD
                     <Route path="/signup" element={<Signup />} />
                     <Route path="/create-event" element={<AddEvent />} />
                     <Route path="/edit-event/:id" element={<Editevent />} />
@@ -38,14 +34,8 @@ function App() {
                     <Route path="/permission-denied" element={<Denied />} />
                     <Route path="/profile" element={<Hostevent />} />
                     <Route path="/Becometohost" element={<Becometohost />} />
-=======
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/create-event" element={<AddEvent />} />
-          <Route path="/edit-event/:id" element={<Editevent />} />
-          <Route path="/events/:id" element={<Event />} />  
           <Route path="/hosts/:id" element={<Host />} />  
           <Route path="/permission-denied" element={<Denied />} />
->>>>>>> cb86ff9f5fcf2a8d64a256b169f9662000383e5d
       </Routes>
       </main>
       
